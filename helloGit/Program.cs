d6c5d4b5329157b7666e0e98cff50c3e223bdf54
Anna Gruber <03gruann@rgtfo-me.it>
2020-04-21 01:04:13 +0200

    output modified

---
+++ helloGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+            Console.ReadKey();
+        }
+    }
+}
